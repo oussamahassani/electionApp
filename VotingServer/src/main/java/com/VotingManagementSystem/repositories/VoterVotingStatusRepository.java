a854b60e56cc54441ca6170384901086384e4626
oussama.hassani <oussama.hassani@chifco.com>
2024-08-26 16:18:00 +0200

    add repository voter

--- VotingServer/src/main/java/com/VotingManagementSystem/repositories/VoterVotingStatusRepository.java
+++ VotingServer/src/main/java/com/VotingManagementSystem/repositories/VoterVotingStatusRepository.java
@@ -1,6 +1,7 @@
 package com.VotingManagementSystem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.VotingManagementSystem.models.election.Election;
 import com.VotingManagementSystem.models.election.VoterVotingStatus;
@@ -13,4 +14,18 @@ public interface VoterVotingStatusRepository extends JpaRepository<VoterVotingSt
     public void deleteByElection(Election election);
 
     public Set<VoterVotingStatus> findAllByElection(Election election);
+    
+    @Query(value = "SELECT user.gender AS gender, COUNT(*) AS count " +
+            "FROM voter_voting_status " +
+            "JOIN user ON votes_counter.user_user_id  = user.user_id  " +
+            "GROUP BY user.gender " +
+            "ORDER BY gender", nativeQuery = true)
+List<Object[]> countVotesByGender();
+
+@Query(value = "SELECT user.gouvernerat AS gouvernerat, COUNT(*) AS count " +
+        "FROM voter_voting_status " +
+        "JOIN user ON votes_counter.user_user_id  = user.user_id  " +
+        "GROUP BY user.gouvernerat " +
+        "ORDER BY gouvernerat", nativeQuery = true)
+List<Object[]> countVotesByGouvernerat();
 }
